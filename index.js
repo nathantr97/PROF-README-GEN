36aeaf8994b07c73cfcd8ce8ba84420bc1e5d830
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-03-02 18:44:09 -0800

    setting up prompts

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs= require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer.prompt = ([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+    },
+    {
+        type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+    },
+    {
+        type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+    
+])
