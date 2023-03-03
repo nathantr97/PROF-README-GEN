b57be9306bc6b7b47394b17021473365161acf98
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-03-02 19:02:11 -0800

    added icons and added prompts with names

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const fs= require('fs');
 
-inquirer.prompt = ([
+inquirer
+.prompt = ([
     {
         type: 'input',
         message: 'What is the title of your project?',
@@ -9,8 +10,34 @@ inquirer.prompt = ([
     },
     {
         type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information for your project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines of your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for your project?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Which license do you want to use?',
+        choices: ["ISC", "MIT", "None"],
+        name: 'license',
     },
     {
         type: 'input',
@@ -19,43 +46,15 @@ inquirer.prompt = ([
     },
     {
         type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
+            message: 'What is the username of your Github?',
+            name: 'github',
     },
     {
         type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-    },
-    {
-        type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-    },
-    {
-        type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-        },
-    
+            message: 'What is your email?',
+            name: 'email',
+    },  
 ])
+
+.then((data)) => {}
+    
